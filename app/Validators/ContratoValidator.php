5772320d17f6ccec7edf9b2728023a05495fee72
Angelo Neto <angelo.neto@fiero.org.br>
2016-06-29 14:10:40 -0400

    Atualização do ContratoValidator

--- app/Validators/ContratoValidator.php
+++ app/Validators/ContratoValidator.php
@@ -8,8 +8,28 @@ use \Prettus\Validator\LaravelValidator;
 class ContratoValidator extends LaravelValidator {
 
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'numero' => 'required|unique:contratos',
+            'ano' => 'required',
+            'casa_id' => 'required',
+            'empresa_id' => 'required',
+            'homologado' => 'required',
+            'executado' => 'required',
+            'data_inicio' => 'required',
+            'data_fim' => 'required',
+            'gestores' => 'required'
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'numero' => 'required',
+            'ano' => 'required',
+            'casa_id' => 'required',
+            'empresa_id' => 'required',
+            'homologado' => 'required',
+            'executado' => 'required',
+            'data_inicio' => 'required',
+            'data_fim' => 'required',
+            'gestores' => 'required'
+        ],
    ];
 
 }
